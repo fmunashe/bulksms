f5d9c20f2d41772010e98bbeb986797abd3a9b03
Farai Zihove <farai.zihove@econet.co.zw>
2024-09-30 16:18:04 +0200

    initial commit

--- routes/console.php
+++ routes/console.php
@@ -2,11 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-//Artisan::command('queue:work',function (){
-//
-//})->everyMinute();
+Schedule::command('queue:work')
+    ->everyFiveSeconds()
+    ->withoutOverlapping();
